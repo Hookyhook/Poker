bc2b4f2c4d53d72d9dac7fcfe878b8b07a5fac14
Hookyhook <108137850+Hookyhook@users.noreply.github.com>
2023-01-25 10:50:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-var deck = [];
+var games = [];
 
-var table;
-gameStart();
+newGame();
 
-function gameStart(){
-    console.log("Starting Game");
-    console.log("Shuffling and Creating deck");
-    createDeck();
-    console.log(deck);
-    console.log("Contributing cards on the table");
-    createTable();
-    console.log(table);
+function newGame(){
+    var deck = createDeck();
+    var table = createTable(deck);
+    var gameid = 1;
+    var creator = 1;
+    var members = [];
+    var member = {meberid: creator, hand: dealHand(deck)};
+    members.push(member);
+    var game = {gameid: gameid, creatorid: creator, members: members, deck: deck, table: table};
+    games.push(game);
+    console.log(games);
 }
 
-
-
 function createDeck(){
-    var colors = ["Karo", "Kreuz", "Blatt", "Herz"];
+    var d = [];
+    var colors = [":diamonds:", ":clubs:", ":spades:", ":hearts:"];
     var numbers = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
     for (const color of colors) {
         for (const number of numbers) {
-            deck.push(color+" "+number);
+            d.push(color+" "+number);
         }
     }
     for (let index = 0; index < 1000; index++) {
-        deck.sort((a,b)=> 0.5-Math.random());
+        d.sort((a,b)=> 0.5-Math.random());
     }
+    return d;
 }
 
-function createTable(){
-    var card1 = deck.pop();
-    var card2 = deck.pop();
-    var card3 = deck.pop();
-    table = {card1, card2, card3};
+function createTable(d){
+    return {card1: d.pop(), card2: d.pop(), card3: d.pop()};
 }
 
-function dealHand(){
-    var card1 = deck.pop();
-    var card2 = deck.pop();
-    return {card1,card2};
+function dealHand(d){
+    return {card1: d.pop(), card2: d.pop()};
+}
+
+function addPlayer(){
+    
 }
